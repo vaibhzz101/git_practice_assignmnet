e99d3395e190bd10d7470048a490b603ad025138
Vaibhav Shevne <vaibhzzsmiley@gmail.com>
2022-09-17 10:53:58 +0530

    added checkPrime problem

---
+++ Prime.js
@@ -0,0 +1,16 @@
+
+
+function identifyPrime(num) 
+   
+    let result="Yes";
+   for(let i=2; i<=num-1; i++){
+       if(num%1==0 ){
+           result=";
+          
+       }
+       
+           
+       }
+       console.log(result)
+       
+   }
